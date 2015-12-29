2adf1f6ae09003b989ae81cee265745f0a0ee697
Raza Balbale <razaibi@gmail.com>
2015-12-29 13:49:48 +0300

    Update Post_WithoutArgs_Sample.js

--- Post_WithoutArgs_Sample.js
+++ Post_WithoutArgs_Sample.js
@@ -1,5 +1,5 @@
 // The Ajax Call
-  var AjaxCaller = rx.zhr("/WebMethodName","ShowResult").dopost();
+  var AjaxCaller = rx.zhr("/WebMethodName","ShowResult","POST").do();
 // Call Ends Here
   
   function ShowResult(ReturnValue){
